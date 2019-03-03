0b81dc6402066f40fbe2f367a956027417be5e9a
Andrew Dow <a.r.dow@ncl.ac.uk>
2019-03-03 07:51:22 +0000

    Final tweaks

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import time
 import os
 from bme280 import getTemp, getHumid, getPress
 
-CSV_FILENAME = 'test.csv'
-JAVASCRIPT_FILENAME = 'web/test.js'
+CSV_FILENAME = 'envLog.csv'
+JAVASCRIPT_FILENAME = 'web/home.js'
 
 #Return current time and date
 def getNow():
@@ -21,10 +21,10 @@ def updateLog():
 #A method to update aws
 def updateAWS():
     writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentTemp").innerHTML = "' + getTemp() +'";', "w")
-    writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentHumid").innerHTML = "' + getHumid() +'";', "a")
-    writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentPress").innerHTML = "' + getPress() +'";', "a")
+    writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentHumid").innerHTML = "' + getHumid()[:5] +'";', "a")
+    writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentPress").innerHTML = "' + getPress()[:6] +'";', "a")
     writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentTime").innerHTML = "' + getNow() +'";', "a")
-    #os.system("aws s3 cp web/home.js s3://andyshoose/home.js" --acl public-read)
+    os.system("aws s3 cp web/home.js s3://andyshoose/home.js" --acl public-read)
 
 #Write data to csv files
 def writeToFile(file, data, permission):
@@ -34,7 +34,7 @@ def writeToFile(file, data, permission):
     f.close()
 
 #A while loop to control the program
-i = 0
+i = 60
 while True:
     #After 60 minutes update aws
     if i == 60:
