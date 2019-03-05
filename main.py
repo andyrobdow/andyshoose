b7382d1a42a35005d837b5bdcd68be62f6468161
Andrew Dow <a.r.dow@ncl.ac.uk>
2019-03-05 07:16:54 +0000

    Running headless version

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 import time
 import os
 from bme280 import getTemp, getHumid, getPress
+import boto3
 
-CSV_FILENAME = 'envLog.csv'
-JAVASCRIPT_FILENAME = 'web/home.js'
+SCRIPT_HOME = '/home/pi/Documents/andyshoose/'
+CSV_FILENAME = SCRIPT_HOME + 'envLog.csv'
+JAVASCRIPT_FILENAME = SCRIPT_HOME + 'web/home.js'
 
 #Return current time and date
 def getNow():
@@ -20,11 +22,13 @@ def updateLog():
 
 #A method to update aws
 def updateAWS():
+    s3 = boto3.resource('s3')
     writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentTemp").innerHTML = "' + getTemp() +'";', "w")
     writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentHumid").innerHTML = "' + getHumid()[:5] +'";', "a")
     writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentPress").innerHTML = "' + getPress()[:6] +'";', "a")
     writeToFile(JAVASCRIPT_FILENAME, 'document.getElementById("currentTime").innerHTML = "' + getNow() +'";', "a")
-    os.system("aws s3 cp web/home.js s3://andyshoose/home.js --acl public-read")
+    data = open(JAVASCRIPT_FILENAME, 'rb')
+    s3.Bucket('andyshoose').put_object(Key='home.js', Body=data, ACL='public-read')
 
 #Write data to csv files
 def writeToFile(file, data, permission):
